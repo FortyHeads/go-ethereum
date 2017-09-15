3dfc8dd7280c7ae166fc0c9c13153141f3ce3426
FortyHeads <dev@fortyheads.com>
2017-09-15 17:30:46 +0300

    stubbed gas calculations

--- core/state_transition.go
+++ core/state_transition.go
@@ -252,6 +252,7 @@ func (st *StateTransition) TransitionDb() (ret []byte, requiredGas, usedGas *big
 			return nil, nil, nil, false, vmerr
 		}
 	}
+	return ret, Big0, Big0, err
 	requiredGas = new(big.Int).Set(st.gasUsed())
 
 	st.refundGas()
